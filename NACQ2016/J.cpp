554880f4d2b3a0a705200871a1fce2ce350142b7
galloska <skap19952008@hotmail.com>
2017-05-05 13:58:41 -0500

    Create J.cpp

---
+++ NACQ2016/J.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define forn(i, n) for(int i=0;i<n;i++)
+#define lli long long int
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define pb push_back
+
+using namespace std;
+
+const int MAXN = 1000001;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    while (n--) {
+        string cad;
+        cin >> cad;
+        cout << cad.size() << '\n';
+    }
+    
+    return 0;
+}
