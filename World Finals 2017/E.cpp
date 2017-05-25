6e601f534faec683f33a8a8fb9126ec826a8ce1e
Jesús Saucedo <jsp.saucedo@gmail.com>
2017-05-24 23:17:02 -0500

    Update E.cpp

--- World Finals 2017/E.cpp
+++ World Finals 2017/E.cpp
@@ -3,43 +3,52 @@
 #define Long long long
 #define forn(i, n) for(int i = 0; i < n; ++i)
 #define rep(i, a, b) for(int i = a; i < b; ++i)
-#define pii pair<int, int>
+#define pii pair<double, double>
 #define fi first
 #define se second
 #define pb push_back
 using namespace std;
 
+double const ERROR = 1e-6;
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int n;
+double t;
+vector< pii > data;
 
-    int n, t;
-    cin >> n >> t;
+double go(double m) {
+    double acc  = 0.0;
+    forn(i, n)
+        acc += data[i].se / (data[i].fi + m);
 
-    vector<double> d(n), s(n);
-    
-    double izq = -1e10;
-    forn(i, n) {
-        cin >> d[i] >> s[i];
-        izq = max(izq, -s[i]);
-    }
+    return acc;
+}
 
-    double der = 1e10;
+double bin(double izq, double der) {
     forn(it, 100) {
         double m = (izq + der) / 2.0;
-    
-        double acc = 0.0;
-        forn(j, n)
-            acc += d[j] / (s[j] + m);
 
-        if (acc > t)
+        if (go(m) > t)
             izq = m;
         else
             der = m;
     }
 
-    cout << fixed << setprecision(9) << (izq + der) / 2.0 << endl;
+    return (izq + der) / 2.0;
+}
 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> t;
+
+    data = vector< pii >(n);
+    
+    forn(i, n)
+        cin >> data[i].se >> data[i].fi;
+
+    sort(data.begin(), data.end());
+
+    cout << fixed << setprecision(9) << bin(-data[0].fi, 1e10) << endl;
     return 0;
 }
